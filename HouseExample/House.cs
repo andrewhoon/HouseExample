c09f92315904a11457f19bae0613c506aedb5c15
andrewhoon <hoonandrew1709@gmail.com>
2017-01-17 15:15:01 -0500

    endofday

--- HouseExample/House.cs
+++ HouseExample/House.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace HouseExample
 {
-    class House
+    class House  //I right clicked on the name on the right (second one down, not solution)
     {
+        private string color;                                   //these are fields
+        private int rooms;           
+        double squareFeet;
+        string metroArea;
+        static private int avgHousePrice;
+
+
+        public void Paint(string color)                         //method (no longer need static, since it is in clear class
+        {                                                       //getting color
+            this.color = color;
+        }
+
+        public string CurrentColor()
+        {                                                               //returning color method. 
+            return color;
+        }
+      
+
+
+
+
+
+
     }
 }
--- HouseExample/Program.cs
+++ HouseExample/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HouseExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            House house1 = new House();           //creating new house.  storing it as houseOne.
+
+            house1.Paint("Green");
+            Console.WriteLine(house1.CurrentColor());  //CurrentColor is method in House.cs
+
+            House house2 = new House();
+            house2.Paint("Blue");
+            Console.WriteLine("House One is " + house1.CurrentColor() + " and House two is " + house2.CurrentColor() + ".");
+
+            Console.WriteLine("I've got a house");
+
+
+
+
+
+
         }
     }
 }
