a08204f73d02eb4ee0e81780999b9ea55de0e498
andrewhoon <hoonandrew1709@gmail.com>
2017-01-18 11:27:50 -0500

    next day morning

--- HouseExample/House.cs
+++ HouseExample/House.cs
@@ -8,28 +8,61 @@ namespace HouseExample
 {
     class House  //I right clicked on the name on the right (second one down, not solution)
     {
-        private string color;                                   //these are fields
-        private int rooms;           
-        double squareFeet;
-        string metroArea;
-        static private int avgHousePrice;
+        string color;                                   //these are fields
+        int rooms;
+        public double squareFeet;
+        public string metroArea;
+        private string name;
 
+        public string Name                 //properties
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
 
-        public void Paint(string color)                         //method (no longer need static, since it is in clear class
-        {                                                       //getting color
+        //    public House (string color, int rooms)
+        //{
+        //    this.color = color;
+        //    this.rooms = rooms;
+        //}
+
+        public House(string color, int rooms, double squareFeet, string metroArea)
+        {
             this.color = color;
+            this.rooms = rooms;
+            this.squareFeet = squareFeet;
+            this.metroArea = metroArea;
+
+
         }
+        public void DisplayHouseInfo()
+        {
+            Console.Write("The color of this house is " + color);
+            Console.Write(", the number of rooms is " + rooms);
+            Console.Write(", the square footage is " + squareFeet);
+            Console.WriteLine(", the metro area is in " + metroArea + ".");
 
-        public string CurrentColor()
-        {                                                               //returning color method. 
-            return color;
+
+            //}
+
+            //public void Paint(string color)                         //method (no longer need static, since it is in clear class
+            //{                                                       //getting color
+            //    this.color = color;
+            //}
+
+            //public string CurrentColor()
+            //{                                                               //returning color method. 
+            //    return color;
+            //}
+
+
+
+
+
+
+
+
         }
-      
-
-
-
-
-
-
     }
 }
+
--- HouseExample/Program.cs
+++ HouseExample/Program.cs
@@ -11,19 +11,29 @@ namespace HouseExample
     {
         static void Main(string[] args)
         {
-            House house1 = new House();           //creating new house.  storing it as houseOne.
+            //House house1 = new House();           //creating new house.  storing it as houseOne.
 
-            house1.Paint("Green");
-            Console.WriteLine(house1.CurrentColor());  //CurrentColor is method in House.cs
+            //house1.Paint("Green");
+            //Console.WriteLine(house1.CurrentColor());  //CurrentColor is method in House.cs
 
-            House house2 = new House();
-            house2.Paint("Blue");
-            Console.WriteLine("House One is " + house1.CurrentColor() + " and House two is " + house2.CurrentColor() + ".");
-
-            Console.WriteLine("I've got a house");
+            //House house2 = new House();
+            //house2.Paint("Blue");
+            //Console.WriteLine("House One is " + house1.CurrentColor() + " and House two is " + house2.CurrentColor() + ".");
 
 
 
+            //House myHouse = new House ("Red", 5, 1700, "Cleveland");
+
+            //myHouse.DisplayHouseInfo();
+
+            //House neighborHouse = new House("Blue", 11, 2400, "Cleveland");
+            //neighborHouse.DisplayHouseInfo();
+
+            House house3 = new House("White", 7, 2400, "Cleveland");
+            house3.DisplayHouseInfo();
+
+            house3.Name = "The White House on the corner";  //calling this from property
+            Console.WriteLine(house3.Name);
 
 
 
